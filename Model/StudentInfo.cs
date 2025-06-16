f3b9f67c3da4f3da19fd01968c293567fe45c980
Saikumar <saikumar.dasari@nexgile.com>
2025-06-16 12:15:43 +0530

    Add project files.

---
+++ Model/StudentInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorld.Model
+{
+    public class StudentInfo
+    {
+        public int Id { get; set; }
+        public  string? StudentName { get; set; }
+        public string? StudentEmail { get; set; }
+        public string? Roll_No { get; set; }
+    }   
+}
